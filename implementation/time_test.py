70f4f481ee5ea2d05cf549bf677124be53db1c37
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-04-10 12:14:52 +0200

    ajouté mini-régression pour déterminer degré polynomial

--- implementation/time_test.py
+++ implementation/time_test.py
@@ -12,6 +12,7 @@ Date: 2021-04-10
 import logging as log
 import time
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 import generateur_ab as ab
@@ -53,12 +54,35 @@ def time_tests(max_size):
     return sizes, gentimes, soltimes
 
 
+def pente(x, y):
+    """Déterminer la pente de la droite de régression."""
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    x_shifted = x - x_mean
+    y_shifted = y - y_mean
+    a = (x_shifted @ y_shifted) / (x_shifted @ x_shifted)
+    return a
+
+
 def analyze(size, gent, solt):
     """Analyzer complexité expérimentale de génération/résolution."""
-    _, axes = plt.subplots()
-    axes.plot(size, gent, "+b", label="generate")
-    axes.plot(size, solt, "+g", label="solve")
-    axes.legend()
+    _, (axes_lin, axes_log) = plt.subplots(1, 2)
+    axes_lin.plot(size, gent, "+b", label="generate")
+    axes_lin.plot(size, solt, "+g", label="solve")
+    axes_lin.set_xlabel("taille de la grille")
+    axes_lin.set_ylabel("durée (s)")
+    axes_lin.legend()
+    axes_log.plot(np.log(size), np.log(gent), "+b", label="generate")
+    axes_log.plot(np.log(size), np.log(solt), "+g", label="solve")
+    axes_log.set_xlabel("log(taille)")
+    axes_log.set_ylabel("log(durée)")
+    axes_log.legend()
+    pente_gen = pente(np.log(size), np.log(gent))
+    pente_sol = pente(np.log(size), np.log(solt))
+    print("le générateur a une complexité expérimentale polynomiale de degré",
+          f"{pente_gen:.3f}")
+    print("le solveur a une complexité expérimentale polynomiale de degré",
+          f"{pente_sol:.3f}")
     plt.show()
 
 
