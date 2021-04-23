fa091a1589e012f1f1469cf3580facbf7d1f55d2
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-04-23 06:37:17 +0200

    possibilité de comparer directement 2 solveurs sur mêmes grilles

--- implementation/time_test.py
+++ implementation/time_test.py
@@ -20,7 +20,7 @@ from solveur_astar_naif import astar as astar_naif
 from solveur_astar_heapq import astar as astar_heapq
 
 
-def single_test(size, solver, ratio_wall_destr=0):
+def single_test(size, solver, solver2=None, ratio_wall_destr=0):
     """Effectuer un test avec une grille aléatoire de la taille donnée."""
     log.debug("starting single test size %d" % size)
     start_time = time.time()
@@ -28,13 +28,20 @@ def single_test(size, solver, ratio_wall_destr=0):
     maze = ab.Maze(size, size, ratio_wall_destr)
     gen_duration = time.time() - start_time
     start_time = time.time()
-    log.debug("solve maze")
+    log.debug("solve maze" + " with solver 1" if solver2 is not None else "")
     solver(maze)
     solve_duration = time.time() - start_time
-    return gen_duration, solve_duration
+    if solver2 is not None:
+        start_time = time.time()
+        log.debug("solve maze with solver 2")
+        solver2(maze)
+        solve_duration2 = time.time() - start_time
+        return gen_duration, solve_duration, solve_duration2
+    else:
+        return gen_duration, solve_duration
 
 
-def time_tests(name, solver, max_size, ratio_wall_destr=0):
+def time_tests(solver, max_size, solver2=None, rwd=0):
     """
     Effectuer des test avec des grilles aléatoires.
 
@@ -43,17 +50,27 @@ def time_tests(name, solver, max_size, ratio_wall_destr=0):
     sizes = []
     gentimes = []
     soltimes = []
+    soltimes2 = []
     size = 10
-    print(f"Starting {name} test with {ratio_wall_destr} wall remove ratio.")
     while size <= max_size:
-        gentime, soltime = single_test(size, solver, ratio_wall_destr)
+        gentime, *soltime = single_test(size, solver, solver2=solver2,
+                                        ratio_wall_destr=rwd)
         sizes.append(size)
         gentimes.append(gentime)
-        soltimes.append(soltime)
-        print(f"{size:4d}x{size:<4d}: {gentime:.4f}s for generate,",
-              f"{soltime:.4f}s for solve")
+        soltimes.append(soltime[0])
+        if solver2 is not None:
+            soltimes2.append(soltime[1])
+            print(f"{size:4d}x{size:<4d}: generate={gentime:.4f}s",
+                  f"solve1={soltime[0]:.4f}s",
+                  f"solve2={soltime[1]:.4f}s for solve2")
+        else:
+            print(f"{size:4d}x{size:<4d}: generate={gentime:.4f}s",
+                  f"solve={soltime[0]:.4f}s")
         size += 10
-    return sizes, gentimes, soltimes
+    if solver2 is not None:
+        return sizes, gentimes, soltimes, soltimes2
+    else:
+        return sizes, gentimes, soltimes
 
 
 def pente(x, y):
@@ -66,31 +83,45 @@ def pente(x, y):
     return res
 
 
-def analyze(size, gent, solt):
+def analyze(size, gent, solt, solt2=None, view=True):
     """Analyzer complexité expérimentale de génération/résolution."""
-    _, (axes_lin, axes_log) = plt.subplots(1, 2)
-    axes_lin.plot(size, gent, "+b", label="generate")
-    axes_lin.plot(size, solt, "+g", label="solve")
-    axes_lin.set_xlabel("taille de la grille")
-    axes_lin.set_ylabel("durée (s)")
-    axes_lin.legend()
-    axes_log.plot(np.log(size), np.log(gent), "+b", label="generate")
-    axes_log.plot(np.log(size), np.log(solt), "+g", label="solve")
-    axes_log.set_xlabel("log(taille)")
-    axes_log.set_ylabel("log(durée)")
-    axes_log.legend()
+    if view:
+        _, (axes_lin, axes_log) = plt.subplots(1, 2)
+        axes_lin.plot(size, gent, "+b", label="generate")
+        axes_lin.plot(size, solt, "+g", label="solve")
+        if solt2 is not None:
+            axes_lin.plot(size, solt2, "+c", label="solve 2")
+        axes_lin.set_xlabel("taille de la grille")
+        axes_lin.set_ylabel("durée (s)")
+        axes_lin.legend()
+        axes_log.plot(np.log(size), np.log(gent), "+b", label="generate")
+        axes_log.plot(np.log(size), np.log(solt), "+g", label="solve")
+        if solt2 is not None:
+            axes_log.plot(np.log(size), np.log(solt2), "+c", label="solve 2")
+        axes_log.set_xlabel("log(taille)")
+        axes_log.set_ylabel("log(durée)")
+        axes_log.legend()
     # la pente log/log est le degré d'une régression monômiale
     # vu que y=cx^d <=> log(x) = d * log(x) + log(c)
     pente_gen = pente(np.log(size), np.log(gent))
-    pente_sol = pente(np.log(size), np.log(solt))
     print("le générateur a une complexité expérimentale polynomiale de degré",
           f"{pente_gen:.3f}")
-    print("le solveur a une complexité expérimentale polynomiale de degré",
+    pente_sol = pente(np.log(size), np.log(solt))
+    print("le solveur 1 a une complexité expérimentale polynomiale de degré",
           f"{pente_sol:.3f}")
+    if solt2 is not None:
+        pente_sol2 = pente(np.log(size), np.log(solt2))
+        print("le solveur 2 a une complexité expérimentale polynomiale",
+              "de degré", f"{pente_sol2:.3f}")
     plt.show()
 
 
 if __name__ == "__main__":
     log.basicConfig(level=log.INFO)
-    analyze(*time_tests("A* naïf", astar_naif, 70, .05))
-    analyze(*time_tests("A* with heapq", astar_heapq, 70, .05))
+    print("* Comparaison de l'effet de la structure de donnée *")
+    print("Algo = A*, SD1 = dict, SD2 = heapq")
+    sz, gent, solt, solt2 = time_tests(astar_naif, 70, solver2=astar_heapq,
+                                       rwd=.05)
+    print("analyzing results with A* with dict vs. heapq")
+    analyze(sz, gent, solt, solt2=solt2)  # , view=False)
+    # analyze(*time_tests(astar_heapq, 70, rwd=.05))
