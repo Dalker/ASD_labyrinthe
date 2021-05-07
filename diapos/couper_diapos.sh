91af06b806c6dee6c24d6fe2ab24061bfd1b7c44
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-05-07 16:54:25 +0200

    convertir en une image par page

--- diapos/couper_diapos.sh
+++ diapos/couper_diapos.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# extraire des "images" du pdf généré par odt
 pdfcrop --margins "-3 -45 -3 -45" ProjetDiaposExplicLab.pdf diapos_coupees.pdf
+
+for page in {1..48}; do
+    pdfjam --outfile page${page}.png diapos_coupees.pdf $page;
+    convert page${page}.pdf page${page}.png;
+    rm page${page}.pdf;
+done
